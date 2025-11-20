19b045bc03b8d4a84333bdd0f38c9583055bd2e2
Luke Opperman <luke@opperman.net>
2025-11-20 12:27:21 -0500

    devices list, key

--- mothmonitor/blueprints/devices.py
+++ mothmonitor/blueprints/devices.py
@@ -1,16 +1,25 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_security import auth_required
-from ..models import db, User
+from ..models import db, Device
 
 devices = Blueprint('devices', __name__)
 
 @devices.route('/list')
 @auth_required()
 def list():
-    return render_template("devices/list.html")
+    devices = db.session.execute(db.select(Device).order_by(Device.id)).scalars()
+    return render_template("devices/list.html", **locals())
 
-@devices.route('/create_key', defaults={"device": None}, methods=["POST"])
-@devices.route('/create_key/<device>', methods=["POST"])
+@devices.route('/create_key', defaults={"device_id": None}, methods=["POST"])
+@devices.route('/create_key/<device_id>', methods=["POST"])
 @auth_required()
-def create_key(device):
-    return render_template("devices/hx/create_key.html", **locals())
+def create_key(device_id):
+    device = Device(name="")
+    if device_id:
+        device = db.get_or_404(Device, device_id)
+    else:
+        db.session.add(device)
+    device.generate_upload_key()
+    db.session.commit()
+    return render_template("devices/hx/row.html", **locals())
+
--- mothmonitor/models.py
+++ mothmonitor/models.py
@@ -23,7 +23,14 @@ class Role(db.Model, fsqla.FsRoleMixin):
 class Device(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(index=True, nullable=True)
-    upload_key: Mapped[str] = mapped_column(index=True, nullable=True,
-                                            default=lambda: secrets.token_hex(16))
+    upload_key: Mapped[str] = mapped_column(index=True, nullable=True)
+    former_keys: Mapped[str] = mapped_column(nullable=True)
     last_seen: Mapped[datetime.datetime] = mapped_column(nullable=True)
 
+    def generate_upload_key(self):
+        if self.upload_key:
+            keys = self.former_keys and [self.former_keys] or []
+            keys.append(self.upload_key)
+            self.former_keys = ",".join(keys)
+        self.upload_key = secrets.token_hex(16)
+        return self.upload_key
